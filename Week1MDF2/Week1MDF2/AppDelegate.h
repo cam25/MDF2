9ac6010032518d06c9e29a425c84d9bc54c218c8
Cameron Mozie <cam25@fullsail.edu>
2013-05-30 15:58:08 -0400

    Initial Commit

---
+++ Week1MDF2/Week1MDF2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Week1MDF2
+//
+//  Created by Cameron Mozie on 5/30/13.
+//  Copyright (c) 2013 Cameron Mozie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
